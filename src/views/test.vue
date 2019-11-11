1ebf0689f2ca564911cb0f251d005a110e8f4e1a
vamsi <gsvnsvk@gmail.com>
2019-11-11 11:40:47 +0530

    updated works page

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="home" ref="home_cont" style="text-align: center;overflow-x: hidden;">
       <p
         class="hoverable home_route_text"
@@ -11,15 +11,19 @@
         style="position: fixed;bottom: 30px; left: 10px;transform: rotate(-90deg);font-weight: 500;padding: 20px;z-index:990;"
       >Contact</p>
       <div ref="overlay" class="overlay"></div>
-  
+      <p class="workText" ref="work1">Hash</p>
 
+
+      <div class="mainBox">
+          <img :src="this.parserImg" ref="mainImg" class="mainImg" alt="">
+      </div>
 
   
     </div>
   </template>
   
   <script>
-  // import VanillaTilt from "vanilla-tilt";
+  import VanillaTilt from "vanilla-tilt";
   
   export default {
     name: "home",
@@ -38,13 +42,14 @@
       };
     },
     mounted: function() {
-      // VanillaTilt.init(this.$refs.main_circle, {
-      //   "full-page-listening": true,
-      //   "max-glare": 1,
-      //   max: 10,
-      //   speed: 100,
-      //   reset: true
-      // });
+      VanillaTilt.init(this.$refs.mainImg, {
+        "full-page-listening": true,
+        "max-glare": 1,
+        max: 10,
+        speed: 100,
+        reset: true
+      });
+      this.setPositions()
       window.addEventListener("scroll", this.mouseIsScrolling);
     },
     updated: function() {
@@ -53,6 +58,19 @@
       window.removeEventListener("scroll", this.mouseIsScrolling);
     },
     methods: {
+      mouseIsScrolling () {
+        console.log('scrolling .........................')
+      },
+      setPositions () {
+        for (let index = 1; index < 6; index++) {
+          if(index == 1) {
+            this.$refs['work' + index].style.top = 'calc(35vh - ' + (this.$refs['work' + index].clientHeight)/2 + 'px)'
+            this.$refs['work' + index].style.left = 'calc(50vw - ' + (this.$refs['work' + index].clientWidth)/2 + 'px)'
+          } else {
+            this.$refs['work' + index].style.top = '-300vh;'
+          }
+        }
+      }
     }
   };
   </script>
@@ -68,5 +86,169 @@
     height: 600vh;
     width: 100vw;
   }
+  .mainBox {
+    position: fixed;
+    z-index: 100;
+    width: 100vw;
+    height: 100vh;
+    top: 0px;
+    left: 0px;
+    background: transparent;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .mainImg {
+    height: 70vmin;
+    width: auto;
+    max-width: 90vmin;
+    align-self: center;
+    margin-left: 20vw;
+    z-index: 985;
+  }
+  /* .workTextCont {
+    position: absolute;
+    width: auto;
+    height: auto;
+    top: 0px;
+    left: 0px;
+    z-index: 980;
+    display: flex;
+    flex-direction: row;
+    background: transparent;
+    justify-content: center;
+  } */
+  .workText{
+    font-size: 8rem;
+    font-weight: 900;
+    margin-left: -20vw;
+    position: fixed;
+    /* left: 30vw;
+    top: 30vh; */
+    z-index: 999;
+  }
 
-  </style>
+  </style> -->
+
+
+
+  <template>
+      <div class="home" ref="home_cont" style="text-align: center;overflow-x: hidden;">
+          <p class="hoverable" @click="$router.push('/    ')"
+              style="position: fixed;top: 20px; right: 20px;transform: rotate(90deg);font-weight: 500;padding: 20px;z-index: 990;">
+              Home</p>
+          <p class="hoverable" @click="$router.push('works')"
+              style="position: fixed;bottom: 30px; left: 10px;transform: rotate(-90deg);font-weight: 500;padding: 20px;z-index:990;">
+              Contact</p>
+          <img class="main_cirle" ref="main_circle" :src="this.hashImg"/>
+          <div class="works_cont" ref="works_cont">
+              <p class="work 1" ref="1">Work 1</p>
+              <p class="work 2" ref="2">Work 2</p>
+              <p class="work 3" ref="3">Work 3</p>
+              <p class="work 4" ref="4">Work 4</p>
+              <p class="work 5" ref="5">Work 5</p>
+              <div style="padding-top: 7vh;padding-bottom: 7vh;"></div>
+          </div>
+      </div>
+  </template>
+  
+  <script>
+      import VanillaTilt from 'vanilla-tilt'
+      export default {
+          name: "home",
+          data() {
+              return {
+                  active: 1,
+                  x : window.innerWidth,
+                  y : window.innerHeight,
+                  hashImg: require('../assets/hash.png'),
+                  sarcImg: require('../assets/sarc.png'),
+                  builderImg: require('../assets/builder.png'),
+                  parserImg: require('../assets/parser.png'),
+                  scpImg: require('../assets/hash.png'),
+              };
+          },
+          mounted: function () {
+              VanillaTilt.init(this.$refs.main_circle, {
+                  "full-page-listening": true,
+                  "max-glare": 1,
+                  max: 10,
+                  speed: 100,
+              });
+              window.addEventListener("scroll", this.mouseIsScrolling);
+              // window.addEventListener('mousemove', this.setTextShadowEvent)
+          },
+          updated: function() {
+              window.addEventListener("scroll", this.mouseIsScrolling);
+          },
+          methods: {
+              setTextShadowEvent () {
+                  this.mouseX = event.pageX;
+                  this.mouseY = event.pageY;
+                  var posY = (0 - (((this.y)/2-this.mouseY)/this.y)*16) + 'px '
+                  var posX = (0 - (((this.x)/2-this.mouseX)/this.x)*16) + 'px '
+                  var tot = posX + posY + '0 #aaa'
+                  this.$refs[this.active].style.textShadow = tot
+              },
+              mouseIsScrolling () {
+                  var leftThresh = window.pageYOffset*25/this.$refs.works_cont.clientHeight
+                  console.log(leftThresh)
+                  this.$refs.works_cont.style.left = -leftThresh+'vw'
+              }
+          }
+      }
+  </script>
+  
+  <style scoped>
+      .main_cirle {
+          position: absolute;
+          position: fixed;
+          top: calc(50vh - 35vmin);
+          left: calc(50vw - 15vmin);
+          height: 70vmin;
+          width: auto;
+          /* background: linear-gradient(45deg, #CB356B, #BD3F32); */
+          /* background: linear-gradient(0deg, #0038b1, #012c88); */
+          transform-style: preserve-3d;
+          transform: perspective(2000px);
+          z-index: 970;
+      }
+      .works_cont {
+          position: absolute;
+          width: 200vw;
+          height: 100vh;
+          top: 0px;
+          left: 0px;
+          z-index: 975;
+          display: flex;
+          flex-direction: column;
+      }
+      .work {
+          font-size: 8rem;
+          color: white;
+          padding: 0px;
+          margin: 0px;
+          font-weight: 900;
+          z-index: 985;
+          padding-top: 20vh;
+          padding-bottom: 20vh;
+          align-self: flex-start;
+      }
+      
+      .work:first-of-type{
+          margin-top: calc(30vh - 4rem);
+          padding-left: 25vw;
+      }
+      .work:nth-of-type(2){
+          padding-left: 40vw;
+      }
+      .work:nth-of-type(3){
+          padding-left:55vw;
+      }
+      .work:nth-of-type(4){
+          padding-left: 70vw;
+      }
+      .work:nth-of-type(5){
+          padding-left: 85vw;
+      }
+  </style> 
--- src/views/works.vue
+++ src/views/works.vue
@@ -1,254 +1,167 @@
-<template>
-  <div class="home" ref="home_cont" style="text-align: center;overflow-x: hidden;">
-    <p
-      class="hoverable home_route_text"
-      @click="$router.push('home')"
-      style="position: fixed;top: 20px; right: 20px;transform: rotate(90deg);font-weight: 500;padding: 20px;z-index: 990;"
-    >Home</p>
-    <p
-      class="hoverable"
-      @click="$router.push('works')"
-      style="position: fixed;bottom: 30px; left: 10px;transform: rotate(-90deg);font-weight: 500;padding: 20px;z-index:990;"
-    >Contact</p>
-    <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-vamsi.appspot.com/o/hash.png?alt=media&token=424452fc-3181-42ee-8eb5-b1234f28a0f1" class="main_cirle" ref="main_circle" />
-    <div ref="overlay" class="overlay"></div>
 
-    <div ref="1" class="worksPlaceholderTextCont">
-        <p class="worksPlaceholderText">Hash</p>
-    </div>
 
-    <div ref="2" class="worksPlaceholderTextCont">
-        <p class="worksPlaceholderText">SARC</p>
-    </div>
-
-    <div ref="3" class="worksPlaceholderTextCont">
-        <p class="worksPlaceholderText">Resume <br> Parser</p>
-    </div>
-
-    <div ref="4" class="worksPlaceholderTextCont">
-        <p class="worksPlaceholderText">Resume <br> Builder</p>
-    </div>
-
-    <div ref="5" class="worksPlaceholderTextCont">
-        <p class="worksPlaceholderText">Smart <br> Car <br> Parking</p>
-    </div>
-
-    <!-- <div class="works_cont" ref="works_cont">
-            <p class="work 1" ref="1">Hash</p>
-            <p class="work 2" ref="2">SARC</p>
-            <p class="work 3" ref="3">Resume <br> Parser</p>
-            <p class="work 4" ref="4">Resume <br> Builder</p>
-            <p class="work 5" ref="5">Smart <br> Car <br> Parking</p>
-            <div style="height: 10vh; width: 50vw; background: white;"></div>
-    </div>-->
-
-  </div>
-</template>
-
-<script>
-import VanillaTilt from "vanilla-tilt";
-
-export default {
-  name: "home",
-  data() {
-    return {
-      active: 1,
-      x: window.innerWidth,
-      y: window.innerHeight,
-      curP: 1,
-      prevP: 0,
-      hashImg: require('../assets/hash.png'),
-      sarcImg: require('../assets/sarc.png'),
-      builderImg: require('../assets/builder.png'),
-      parserImg: require('../assets/parser.png'),
-      scpImg: require('../assets/hash.png'),
-    };
-  },
-  mounted: function() {
-    VanillaTilt.init(this.$refs.main_circle, {
-      "full-page-listening": true,
-      "max-glare": 1,
-      max: 10,
-      speed: 100,
-      reset: true
-    });
-    this.setMargin()
-    window.addEventListener("scroll", this.mouseIsScrolling);
-  },
-  updated: function() {
-    // this.mouseIsScrolling()
-    // window.addEventListener("scroll", this.mouseIsScrolling);
-  },
-  destroyed: function() {
-    window.removeEventListener("scroll", this.mouseIsScrolling);
-  },
-  methods: {
-    setTextShadowEvent() {
-      this.mouseX = event.pageX;
-      this.mouseY = event.pageY;
-      var posY = 0 - ((this.y / 2 - this.mouseY) / this.y) * 16 + "px ";
-      var posX = 0 - ((this.x / 2 - this.mouseX) / this.x) * 16 + "px ";
-      var tot = posX + posY + "0 #aaa";
-      this.$refs[this.active].style.textShadow = tot;
-    },
-    mouseIsScrolling () {
-        var scrollRatio = window.pageYOffset / window.innerHeight
-        if(scrollRatio < 1 && scrollRatio >= 0) {
-            this.$refs.main_circle.style.transitionDelay = '0.5s'
-            this.$refs.main_circle.style.opacity = '0'
-            this.$refs.main_circle.src = this.hashImg
-            this.$refs.main_circle.style.opacity = '1'
-            this.$refs.main_circle.style.transitionDelay = '0s'
-        } else {
-            if(scrollRatio > 1 && scrollRatio < 2) {
-                this.$refs.main_circle.style.transitionDelay = '0.5s'
-                this.$refs.main_circle.style.opacity = '0'
-                this.$refs.main_circle.src = this.sarcImg
-                this.$refs.main_circle.style.opacity = '1'
-                this.$refs.main_circle.style.transitionDelay = '0s'
-            } else {
-                if(scrollRatio > 2 && scrollRatio < 3) {
-                    this.$refs.main_circle.style.transitionDelay = '0.5s'
-                    this.$refs.main_circle.style.opacity = '0'
-                    this.$refs.main_circle.src = this.builderImg
-                    this.$refs.main_circle.style.opacity = '1'
-                    this.$refs.main_circle.style.transitionDelay = '0s'
-                } else {
-                    if(scrollRatio > 3 && scrollRatio < 4) {
-                        this.$refs.main_circle.style.transitionDelay = '0.5s'
-                        this.$refs.main_circle.style.opacity = '0'
-                        this.$refs.main_circle.src = this.parserImg
-                        this.$refs.main_circle.style.opacity = '1'
-                        this.$refs.main_circle.style.transitionDelay = '0s'
+  <template>
+        <div class="home" ref="home_cont" style="text-align: center;overflow-x: hidden;">
+            <p class="hoverable" @click="$router.push('/    ')"
+                style="position: fixed;top: 20px; right: 20px;transform: rotate(90deg);font-weight: 500;padding: 20px;z-index: 990;">
+                Home</p>
+            <p class="hoverable" @click="$router.push('works')"
+                style="position: fixed;bottom: 30px; left: 10px;transform: rotate(-90deg);font-weight: 500;padding: 20px;z-index:990;">
+                Contact</p>
+            <img class="main_cirle" ref="main_circle" :src="this.hashImg"/>
+            <div class="works_cont" ref="works_cont">
+                <p class="work 1" ref="1">Hash</p>
+                <p class="work 2" ref="2">SARC</p>
+                <p class="work 3" ref="3">Resume <br> Builder</p>
+                <p class="work 4" ref="4">Resume <br> Parser</p>
+                <p class="work 5" ref="5">Smart <br> Car <br> Parking</p>
+                <div style="padding-top: 7vh;padding-bottom: 7vh;"></div>
+            </div>
+        </div>
+    </template>
+    
+    <script>
+        import VanillaTilt from 'vanilla-tilt'
+        export default {
+            name: "home",
+            data() {
+                return {
+                    active: 1,
+                    x : window.innerWidth,
+                    y : window.innerHeight,
+                    hashImg: require('../assets/hash.png'),
+                    sarcImg: require('../assets/sarc.png'),
+                    builderImg: require('../assets/builder.png'),
+                    parserImg: require('../assets/parser.png'),
+                    scpImg: require('../assets/hash.png'),
+                    counter : 0,
+                };
+            },
+            mounted: function () {
+                this.initTilt()
+                window.addEventListener("scroll", this.mouseIsScrolling);
+                // window.addEventListener('mousemove', this.setTextShadowEvent)
+            },
+            updated: function() {
+                window.addEventListener("scroll", this.mouseIsScrolling);
+            },
+            methods: {
+                initTilt () {
+                    VanillaTilt.init(this.$refs.main_circle, {
+                        "full-page-listening": true,
+                        "max-glare": 1,
+                        max: 10,
+                        speed: 100,
+                    });
+                },
+                destroyTilt () {
+                    this.$refs.main_circle.vanillaTilt.destroy()
+                },
+                setTextShadowEvent () {
+                    this.mouseX = event.pageX;
+                    this.mouseY = event.pageY;
+                    var posY = (0 - (((this.y)/2-this.mouseY)/this.y)*16) + 'px '
+                    var posX = (0 - (((this.x)/2-this.mouseX)/this.x)*16) + 'px '
+                    var tot = posX + posY + '0 #aaa'
+                    this.$refs[this.active].style.textShadow = tot
+                },
+                mouseIsScrolling () {
+                    var leftThresh = window.pageYOffset*25/this.$refs.works_cont.clientHeight
+                    console.log((window.pageYOffset/ this.y))
+                    this.$refs.works_cont.style.left = -leftThresh+'vw'
+                    // if((window.pageYOffset / this.y) > 0.4) {
+                    //     try {
+                    //         this.destroyTilt()
+                    //     }
+                    //     catch (err) {
+                    //         console.log('no tilt')
+                    //     }
+                    //     this.$refs.main_circle.style.transition = '0.2s ease-in-out'
+                    //     this.$refs.main_circle.style.opacity = '0'
+                    //     this.$refs.main_circle.style.transition = '0s ease-in-out'
+                    // } else {
+                    //     this.$refs.main_circle.style.transition = '0.3s ease-in-out'
+                    //     this.$refs.main_circle.style.opacity = '1'
+                    //     this.$refs.main_circle.style.transition = '0s ease-in-out'
+                    //     this.initTilt()
+                    // }
+                    var scrollPer = window.pageYOffset / this.y
+                    if(scrollPer <= 0.42) {
+                        this.$refs.main_circle.src = this.hashImg
                     } else {
-                        this.$refs.main_circle.style.transitionDelay = '0.5s'
-                        this.$refs.main_circle.style.opacity = '0'
-                        this.$refs.main_circle.src = this.scpImg
-                        this.$refs.main_circle.style.opacity = '1'
-                        this.$refs.main_circle.style.transitionDelay = '0s'
+                        if(scrollPer <= 1 && scrollPer > 0.48) {
+                            this.$refs.main_circle.src = this.sarcImg
+                        } else {
+                            if(scrollPer <= 1.56 && scrollPer > 1) {
+                                this.$refs.main_circle.src = this.builderImg
+                            } else {
+                                if(scrollPer > 2 && scrollPer <= 2.4) {
+                                    this.$refs.main_circle.src = this.parserImg
+                                } else {
+                                    if(scrollPer > 2.4) {
+                                        this.$refs.main_circle.src = this.hashImg
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-        console.log(this.prevP)
-
-    },
-    setMargin() {
-        for (let index = 1; index < 6; index++) {
-            if(this.curP == index) {
-                this.$refs[index].style.display = 'flex'
-            } else {
-                this.$refs[index].style.display = 'none'
-            }            
-        }
-    }
-  }
-};
-</script>
-
-<style scoped>
-.main_cirle {
-  position: fixed;
-  top: 20vh;
-  left: auto;
-  right: auto;
-  height: 60vh;
-  width: auto;
-  /* background: linear-gradient(45deg, #CB356B, #BD3F32); */
-  /* background: linear-gradient(0deg, #0038b1, #012c88); */
-  transform-style: preserve-3d;
-  transform: perspective(1000px);
-  z-index: 0;
-}
-.overlay {
-  position: absolute;
-  z-index: 90;
-  background: transparent;
-  top: 0px;
-  left: 0px;
-  height: 600vh;
-  width: 100vw;
-}
-.worksPlaceholderTextCont {
-    position: fixed;
-    left: -10vw;
-    top: 20vh;
-    width: 100vw;
-    height: 60vh;
-    display: none;
-    flex-direction: column;
-    justify-content: center;
-}
-.worksPlaceholderText {
-    text-align: left;
-    align-self: center;
-    font-size: 8rem;
-    color: white;
-    text-shadow: 2px 0px #fff;
-    font-weight: 900;
-}
-
-/* 
-    .works_cont {
-        width: 200vw;
-        height: auto;
-        z-index: 975 !important;
-        display: flex;
-        flex-direction: column;
-    }
-    .work {
-        font-size: 8rem;
-        color: white;
-        padding: 0px;
-        margin: 0px;
-        font-weight: 900;
-        z-index: 985;
-        padding-top: 20vh;
-        padding-bottom: 20vh;
-        align-self: flex-start;
-        text-align: left;
-    }
+    </script>
     
-    .work:first-of-type{
-        margin-top: calc(30vh - 4rem);
-        padding-left: 25vw;
-    }
-    .work:nth-of-type(2){
-        padding-left: 40vw;
-    }
-    .work:nth-of-type(3){
-        padding-left:55vw;
-    }
-    .work:nth-of-type(4){
-        padding-left: 70vw;
-    }
-    .work:nth-of-type(5){
-        padding-left: 85vw;
-    }
-    @media screen and (max-width: 768px) {
-        .work {
-            font-size: 4rem;
+    <style scoped>
+        .main_cirle {
+            position: absolute;
+            position: fixed;
+            top: calc(50vh - 35vmin);
+            left: calc(50vw - 15vmin);
+            height: 70vmin;
+            width: auto;
+            /* background: linear-gradient(45deg, #CB356B, #BD3F32); */
+            /* background: linear-gradient(0deg, #0038b1, #012c88); */
+            transform-style: preserve-3d;
+            transform: perspective(2000px);
+            z-index: 970;
         }
+        .works_cont {
+            position: absolute;
+            width: 200vw;
+            height: 100vh;
+            top: 0px;
+            left: 0px;
+            z-index: 975;
+            display: flex;
+            flex-direction: column;
+        }
+        .work {
+            font-size: 8rem;
+            color: white;
+            padding: 0px;
+            margin: 0px;
+            font-weight: 900;
+            z-index: 985;
+            padding-top: 20vh;
+            padding-bottom: 20vh;
+            align-self: flex-start;
+            text-align: left;
+        }
+        
         .work:first-of-type{
-            margin-top: calc(30vh - 2rem);
+            margin-top: calc(30vh - 4rem);
             padding-left: 25vw;
         }
-    }
-    @media screen and (max-width: 460px) {
-        .hoverable {
-            font-size: 20px;
+        .work:nth-of-type(2){
+            padding-left: 40vw;
         }
-        .contact_overlay {
-            width: 55px;
-            height: 140px;
-            top: calc(410vh - 140px);
+        .work:nth-of-type(3){
+            padding-left:55vw;
         }
-        .work {
-            font-size: 2rem;
-            padding-top: 40vh;
+        .work:nth-of-type(4){
+            padding-left: 70vw;
         }
-        .work:first-of-type{
-            margin-top: calc(10vh - 2rem);
+        .work:nth-of-type(5){
+            padding-left: 85vw;
         }
-    } */
-</style>
+    </style> 
+  
